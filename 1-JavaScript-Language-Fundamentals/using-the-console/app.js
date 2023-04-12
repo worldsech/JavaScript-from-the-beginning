d45ae45956f05d1d273c3770453ecb915ac4ba4e
worldsech <emmanuelamadi111@gmail.com>
2023-04-12 09:27:13 +0100

    first-commit

---
+++ 1-JavaScript-Language-Fundamentals/using-the-console/app.js
@@ -0,0 +1,23 @@
+//log to console
+/*
+multi
+line
+comments
+*/
+console.log('hello world');
+console.log(1234);
+console.log(true);
+console.log([1,2,3,4]);
+console.log({a:1, b:4, c:8});
+console.table({a:3, b:5, c:9});
+console.error('this is an error');
+console.clear();
+console.warn('this is a warning');
+console.time('hello');
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+console.timeEnd('hello');
